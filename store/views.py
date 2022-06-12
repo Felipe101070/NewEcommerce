dca1ff7adeb97809bb358082d745c48c4b00d9fb
Felipe Araujo <felipej.araujo@gmail.com>
2022-06-12 02:32:40 -0300

    segundo commit

--- store/views.py
+++ store/views.py
@@ -16,19 +16,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
-        # paginator = Paginator(products, 1)
-        # page = request.GET.get('page')
-        # paged_products = paginator.get_page(page)
     else:  
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        # paginator = Paginator(products, 3)
-        # page = request.GET.get('page')
-        # paged_products = paginator.get_page(page)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     
@@ -38,13 +38,13 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        #in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
-        #'in_cart':in_cart,
+        'in_cart':in_cart,
     }
     return render(request, 'store/product_detail.html', context)
 
